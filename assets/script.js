a06d3c07cf40c1d587a47be82879d825506d7ee8
Adrian2C <adriancoceres.93@gmail.com>
2023-01-31 15:08:51 -0300

    fin de ticmas

--- assets/script.js
+++ assets/script.js
@@ -1,77 +1,45 @@
-/*============== SHOW SIDEBAR ================*/
+/*============== pick elementos ================*/
 const navMenu = document.getElementById("sidebar"),
   navToggle = document.getElementById("nav-toggle"),
   navClose = document.getElementById("nav-close");
 
-/*===============SIDEBAR SHOW================= */
-/*Validate if constant Exist */
+/*===============Show barra lateral================= */
+
 if (navToggle) {
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-sidebar");
   });
 }
 
-/*===============SIDEBAR HIDDEN================= */
+/*===============hide barra lateral================= */
 if (navClose) {
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-sidebar");
   });
 }
 
-/*===============SKILLS TAB================= */
+/*===============Animacion de menu ================= */
 
-const tabs = document.querySelectorAll('[data-target]'),
-    tabContent = document.querySelectorAll('[data-content]')
-
-    tabs.forEach(tab => {
-        tab.addEventListener("click", () => {
-            const target = document.querySelector(tab.dataset.target)
-            
-            tabContent.forEach(tabContents =>{
-                tabContents.classList.remove('skills__active')
-            })
-
-            target.classList.add('skills__active')
-
-
-            tabs.forEach(tab => {
-                tab.classList.remove('skills__active')
-            })
-
-            tab.classList.add('skills__active')
-    })
-})
-
-/*===============SCROLL SECTION ACTIVE LINK================= */
-
-//esto hace que el bullet de la barra de navegacion lateral vaya acorde con la seccion de la pagina donde se encuentre el usuario
-//se va a ir corriendo sola generando un "active", en cada seccion a la que se vaya accediendo
-
-
-//get all sections that have an id dwefined
 const sections = document.querySelectorAll("section[id]");
-//add en event listener listening for scroll
 
 window.addEventListener("scroll", navHighlighter);
 function navHighlighter() {
-    //get current scroll position
-    let scrollY = window.pageYOffset;
-    //now we loop through sections to get height, top and ID values for each
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop -50,
-        sectionId = current.getAttribute("id");
-        /* - if our currrent scroll position enters the space where current secion on screen is, add .active class to corresponding
-        navigation link, eslse remove it.
-        - to know which link needs an active class, we use sectionID variable we are getting while looping through
-        sections as an selector*/
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight)
-        {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add("active-link")
-        }
-        else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove("active-link")
-        }
-    })
+  let scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
 }
-/*===============SIDEBARSHOW=================*/
+/*===============fin de animacion=================*/
